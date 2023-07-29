a03267939c3df29845dd72df2964ca9d8455a50d
Naman Kaushik <kaushiknaman.dk@gmail.com>
2023-07-30 00:40:55 +0530

    brochure update

--- src/components/Brochure.js
+++ src/components/Brochure.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import brochure_pdf from './flyer_fellowship.pdf'
+import brochure_pdf from './brochure.pdf'
 export default function Brochure() {
     return (
 
-        <a href={Pdf} target="_blank">
+        <a href={brochure_pdf} target="_blank">
             <div className="z-20 -translate-x-[3.6rem] fixed top-1/2 left-0 rotate-[90deg] translate-y-1/2 p-4 rounded-t-lg bg-teal-500 text-white ">
                 Download Brochure
             </div>
