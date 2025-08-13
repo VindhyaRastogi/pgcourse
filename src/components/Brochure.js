a13898fabf240145b542177dd4073a369b0d04da
vindhyarastogi3 <vindhyarastogi3@iiitd.edu.in>
2025-08-13 15:58:36 +0530

    brochure updated

--- src/components/Brochure.js
+++ src/components/Brochure.js
@@ -1,13 +1,11 @@
-import React from 'react'
-import brochure_pdf from './brochure.pdf'
+import React from "react";
+import updatedbrochure_pdf from "./updated_brochure.pdf";
 export default function Brochure() {
-    return (
-
-        <a href={brochure_pdf} target="_blank">
-            <div className="z-20 -translate-x-[3.6rem] fixed top-1/2 left-0 rotate-[90deg] translate-y-1/2 p-4 rounded-t-lg bg-teal-500 text-white ">
-                Download Brochure
-            </div>
-        </a>
-    );
-
+  return (
+    <a href={updatedbrochure_pdf} target="_blank">
+      <div className="z-20 -translate-x-[3.6rem] fixed top-1/2 left-0 rotate-[90deg] translate-y-1/2 p-4 rounded-t-lg bg-teal-500 text-white ">
+        Download Brochure
+      </div>
+    </a>
+  );
 }
