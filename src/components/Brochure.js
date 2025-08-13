f1ecf8626a4e274d6241be49e5aca4a45ec3dabd
vindhyarastogi3 <vindhyarastogi3@iiitd.edu.in>
2025-08-13 16:35:43 +0530

    adjusted positioning of regulation

--- src/components/Brochure.js
+++ src/components/Brochure.js
@@ -3,7 +3,7 @@ import updatedbrochure_pdf from "./updated_brochure.pdf";
 export default function Brochure() {
   return (
     <a href={updatedbrochure_pdf} target="_blank">
-      <div className="z-20 -translate-x-[3.6rem] fixed top-1/2 left-0 rotate-[90deg] translate-y-1/2 p-4 rounded-t-lg bg-teal-500 text-white ">
+      <div className="z-20 -translate-x-[3.6rem] fixed top-1/4 left-0 rotate-[90deg] translate-y-[110%] p-4 rounded-t-lg bg-teal-500 text-white ">
         Download Brochure
       </div>
     </a>
--- src/components/Regulation.js
+++ src/components/Regulation.js
@@ -3,7 +3,7 @@ import regulations_pdf from "./Regulations.pdf";
 export default function Regulation() {
   return (
     <a href={regulations_pdf} target="_blank">
-      <div className="z-20 -translate-x-[3.6rem] fixed top-1/4 left-6 rotate-[90deg] translate-y-1/2 p-4 rounded-t-lg bg-teal-500 text-white ">
+      <div className="z-20 -translate-x-[3.6rem] fixed top-1/2 left-6 rotate-[90deg] translate-y-[100%] p-4 rounded-t-lg bg-teal-500 text-white ">
         Regulations
       </div>
     </a>
